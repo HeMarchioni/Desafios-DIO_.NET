b655f3520ec4ea2c6d594906b6a6136c8cad49b5
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-01-28 13:45:17 -0300

    Desafio Multiplos concluido

---
+++ Desafios-DIO_.NET - Multiplos/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// Desafios Iniciais - GFT Start #3 .NET - Múltiplos
+// Realizado por: Henrique Marchioni - 28/01/2022
+
+string[] valores = Console.ReadLine().Split(' ');
+int A = int.Parse(valores[0]);
+
+int B = int.Parse(valores[1]);
+
+if (A % B  == 0 || B % A == 0)
+{
+    Console.WriteLine("Sao Multiplos");
+}
+else
+{
+    Console.WriteLine("Nao sao Multiplos");
+}
