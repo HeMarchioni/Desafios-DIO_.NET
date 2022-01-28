cb51ace7bb284b3a3de0360ac7acf1222b589f2c
Henrique Santos Marchioni <henrique.marchioni@modalgr.com.br>
2022-01-28 13:07:34 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios-DIO_.NET/Diferenca.cs
@@ -0,0 +1,12 @@
+﻿
+
+// Desafio - Diferença
+
+int A = int.Parse(Console.ReadLine());
+int B =
+int C = 
+int D = 
+         
+        //Declare suas variáveis nos espaços em branco
+            Console.WriteLine(            );
+Console.ReadKey();
