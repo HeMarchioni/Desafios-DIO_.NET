fcf3703ed2f39951b7102a01bda1869981d7ff7a
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-01-28 13:35:46 -0300

    Desafio diferença concluido

--- Desafios-DIO_.NET/Diferenca.cs
+++ Desafios-DIO_.NET/Diferenca.cs
@@ -1,12 +1,15 @@
 ﻿
 
-// Desafio - Diferença
+// Desafios Iniciais - GFT Start #3 .NET - Diferença
+// Realizado por: Henrique Marchioni - 28/01/2022
 
 int A = int.Parse(Console.ReadLine());
-int B =
-int C = 
-int D = 
-         
-        //Declare suas variáveis nos espaços em branco
-            Console.WriteLine(            );
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int D = int.Parse(Console.ReadLine());
+
+
+int diferenca = (A * B) - (C * D); 
+
+Console.WriteLine($"DIFERENCA = {diferenca}");
 Console.ReadKey();
