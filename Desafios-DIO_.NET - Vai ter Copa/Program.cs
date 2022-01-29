f3c9dbde48859043f4b7dcd0181bb5aaacf8da24
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-01-29 02:22:39 -0300

    Desafio Vai ter copa concluido

---
+++ Desafios-DIO_.NET - Vai ter Copa/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Desafios Iniciais - GFT Start #3 .NET - Vai Ter Copa?
+// Realizado por: Henrique Marchioni - 29/01/2022
+
+string str;
+while ((str = Console.ReadLine()) != null)
+{
+   
+        int x = int.Parse(str);
+        if (x != 0)
+        {
+            Console.WriteLine("vai ter duas!");
+        }
+        else
+        {
+            Console.WriteLine("vai ter copa!");
+        }
+    
+}
