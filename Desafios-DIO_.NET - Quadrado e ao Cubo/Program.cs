7e7eeef8a822d115d293644ebc7c682ea1878b19
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-02 11:53:09 -0300

    Desafio - Quadrado e ao Cubo concluido

---
+++ Desafios-DIO_.NET - Quadrado e ao Cubo/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Desafios Iniciais - GFT Start #4 .NET - Quadrado e ao Cubo
+// Realizado por: Henrique Marchioni - 02/02/2022
+
+
+int n = int.Parse(Console.ReadLine());
+
+
+
+for (int i = 1; i <= n; i++)
+{
+    int inicio = i;
+
+    Console.Write(inicio);
+    inicio = inicio * i;
+    Console.Write(" "+inicio);
+    inicio = inicio * i;
+    Console.WriteLine(" "+inicio);
+}
