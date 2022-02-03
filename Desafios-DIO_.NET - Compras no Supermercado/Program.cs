6746d3b3a8f33e847071fd97cb1a46f379614e7d
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-03 11:11:43 -0300

    Desafio - Compras no Supermercado concluido

---
+++ Desafios-DIO_.NET - Compras no Supermercado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Desafios Iniciais - GFT Start #4 .NET - Compras no Supermercado
+// Realizado por: Henrique Marchioni - 03/02/2022
+
+
+int totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < totalDeCasosDeTeste; i++)
+{
+
+    List<string> lista = Console.ReadLine().Split(' ').Distinct().ToList();
+
+    lista.Sort();
+
+    foreach (var item in lista)
+    {
+        Console.Write($"{item} ");
+    }
+
+    Console.WriteLine();
+    
+}
