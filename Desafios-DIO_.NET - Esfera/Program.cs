d921b72970a2c6892bd3105c7a876bd522f7d212
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-28 17:18:15 -0300

    Desafio Esfera Concluido

---
+++ Desafios-DIO_.NET - Esfera/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+// Desafios C# - LocalizaLabs .NET Developer #2 - Esfera
+// Realizado por: Henrique Marchioni - 28/02/2022
+
+
+const double pi = 3.14159;
+
+double raio = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("VOLUME = "+((4.0 / 3)*pi*(Math.Pow(raio, 3))).ToString("0.000"));
