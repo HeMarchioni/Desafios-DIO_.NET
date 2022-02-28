5805dd0831bbb84f4ea37bd446a1cb81719f06c4
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-28 18:21:43 -0300

    Desafio Concluido Xenlonguinho

---
+++ Desafios-DIO_.NET - Xenlonguinho/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Desafios C# - LocalizaLabs .NET Developer #2 - Xenlonguinho
+// Realizado por: Henrique Marchioni - 28/02/2022
+
+
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numero; i++)
+{
+
+
+    int esferas = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(esferas - (((int)Math.Sqrt(esferas))));   
+  
+
+}
+                
+
+
+
+            
