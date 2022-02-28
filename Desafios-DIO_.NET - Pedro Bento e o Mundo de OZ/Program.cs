a254cff4aa448b881e0eb220037384fa71ee4d7e
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-28 19:17:05 -0300

    Desafio Pedro Bento e o Mundo de OZ Concluido

---
+++ Desafios-DIO_.NET - Pedro Bento e o Mundo de OZ/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+// Desafios C# - LocalizaLabs .NET Developer #2 - Pedro Bento e o Mundo de OZ
+// Realizado por: Henrique Marchioni - 28/02/2022
+
+
+List<string> joias = new List<string>();
+
+
+
+do
+{
+
+    joias.Add(Console.ReadLine());
+
+} while (!string.IsNullOrEmpty(joias[joias.Count - 1]));
+        {
+
+            var unijoias = joias.SkipLast(1).Distinct();
+
+            System.Console.WriteLine(unijoias.Count());
+
+        };
+    
+      
